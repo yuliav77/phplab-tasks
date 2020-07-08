272de5425ee93bc2a14a5bb75e69930e90e9f896
Maksym Odanets <Maksym.Odanets@gmail.com>
2020-07-08 11:41:48 +0300

    typo fixes

--- src/02-strings.php
+++ src/02-strings.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * The $input variable contains text in snake case (i.e. hello_world or this_is_home_task)
- * Transform it into camel cased string and return (i.e. helloWorld or thisIsHomeTask)
+ * Transform it into a camel-cased string and return (i.e. helloWorld or thisIsHomeTask)
  * @see http://xahlee.info/comp/camelCase_vs_snake_case.html
  *
  * @param  string  $input
@@ -25,7 +25,7 @@ function mirrorMultibyteString(string $input)
 
 /**
  * My friend wants a new band name for her band.
- * She likes bands that use the formula: 'The' + a noun with first letter capitalized.
+ * She likes bands that use the formula: 'The' + a noun with the first letter capitalized.
  * However, when a noun STARTS and ENDS with the same letter,
  * she likes to repeat the noun twice and connect them together with the first and last letter,
  * combined into one word like so (WITHOUT a 'The' in front):
