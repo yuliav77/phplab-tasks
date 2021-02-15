70cbe0a6a7b4bdd6613f07cedc451a42abef52e5
Maksym Odanets <Maksym.Odanets@gmail.com>
2021-02-15 14:17:35 +0200

    DB task

--- src/db/import.php
+++ src/db/import.php
@@ -3,7 +3,7 @@
  * TODO
  *  Open web/airports.php file
  *  Go through all airports in a loop and INSERT airports/cities/states to equivalent DB tables
- *  (make sure, that you do not INSERT the same values to the cities and states table i.e. name should be unique i.e. before INSERTing check if record exists)
+ *  (make sure, that you do not INSERT the same values to the cities and states i.e. name should be unique i.e. before INSERTing check if record exists)
  */
 
 /** @var \PDO $pdo */
